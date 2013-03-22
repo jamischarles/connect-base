35063618eca82107f6aaaa7c935a84e95bc7ee5b
Cameron Bytheway <cameron@nujii.com>
2013-03-22 15:33:29 -0600

    Rename header vars

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ module.exports = function(options) {
   options = options || {};
   
   // Header names
-  var host = options.host || 'x-forwarded-host'
-    , path = options.path || 'x-forwarded-path'
-    , port = options.port || 'x-forwarded-port'
-    , proto = options.proto || 'x-forwarded-proto';
+  var hostHeader = options.host || 'x-forwarded-host'
+    , pathHeader = options.path || 'x-forwarded-path'
+    , portHeader = options.port || 'x-forwarded-port'
+    , protoHeader = options.proto || 'x-forwarded-proto';
   
   return function base(req, res, next) {
     var hostParts = req.headers.host.split(":")
-      , host = req.headers[host] || hostParts[0] || ""
-      , path = req.headers[path] || ""
-      , protocol = req.headers[proto] || req.protocol || "http"
-      , port = req.headers[port] || hostParts[1] || "";
+      , host = req.headers[hostHeader] || hostParts[0] || ""
+      , path = req.headers[pathHeader] || ""
+      , protocol = req.headers[protoHeader] || req.protocol || "http"
+      , port = req.headers[portHeader] || hostParts[1] || "";
 
     if((port == 80 && protocol === "http") || (port == 443 && protocol === "https")) port = "";
     if(port) port = ":"+port;
