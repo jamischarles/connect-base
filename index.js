d7508d5499cd43190257a5646a883b11d6406b54
Cameron Bytheway <cameron@nujii.com>
2013-03-22 15:30:27 -0600

    Allow header names to be set

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-
-module.exports = function() {
+module.exports = function(options) {
+  options = options || {};
+  
+  // Header names
+  var host = options.host || 'x-forwarded-host'
+    , path = options.path || 'x-forwarded-path'
+    , port = options.port || 'x-forwarded-port'
+    , proto = options.proto || 'x-forwarded-proto';
+  
   return function base(req, res, next) {
     var hostParts = req.headers.host.split(":")
-      , host = req.headers['x-forwarded-host'] || hostParts[0] || ""
-      , path = req.headers['x-forwarded-path'] || ""
-      , protocol = req.headers['x-forwarded-proto'] || req.protocol || "http"
-      , port = req.headers['x-forwarded-port'] || hostParts[1] || null;
+      , host = req.headers[host] || hostParts[0] || ""
+      , path = req.headers[path] || ""
+      , protocol = req.headers[proto] || req.protocol || "http"
+      , port = req.headers[port] || hostParts[1] || "";
 
     if((port == 80 && protocol === "http") || (port == 443 && protocol === "https")) port = "";
     if(port) port = ":"+port;
