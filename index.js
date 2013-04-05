49bd8ea8f92a73ef23aeeab731acafdc1783b564
Cameron Bytheway <cameron@nujii.com>
2013-04-05 13:56:32 -0600

    Better url resolving

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Module dependencies
  */
 var url = require("url")
+  , resolve = require("path").resolve
   , debug = require("debug")("connect-base");
 
 module.exports = function(options) {
@@ -16,12 +17,15 @@ module.exports = function(options) {
   return function base(req, res, next) {
     var hostParts = req.headers.host.split(":");
 
+    // Construct the base
     var base = {
       protocol: req.headers[protoHeader] || req.protocol || "http",
       hostname: req.headers[hostHeader] || hostParts[0] || "",
       port: req.headers[portHeader] || hostParts[1] || "",
       pathname: req.headers[pathHeader] || ""
     };
+
+    // Remove standard ports
     if((base.port == 80 && base.protocol === "http") ||
        (base.port == 443 && base.protocol === "https")) delete base.port;
 
@@ -32,8 +36,19 @@ module.exports = function(options) {
 
     // Expose req.resolve
     req.resolve = function(path) {
-      var resolvedPath = url.resolve(req.base, path);
-      debug(resolvedPath);
+      // It's an absolue path
+      if(url.parse(path).protocol) return path;
+
+      // It's a relative path
+      var resolvedPath = url.format({
+        protocol: base.protocol,
+        hostname: base.hostname,
+        port: base.port,
+        pathname: resolve(base.pathname || "/", path)
+      });
+
+      debug("Resolving path",base.pathname || "/","+",path,"->",resolvedPath);
+
       return resolvedPath;
     };
     next();
